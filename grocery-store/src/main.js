534a9b3d5f692d9d88060f47e9fa030b3d368cc1
Caroline Jarman <carolinerosejarman@gmail.com>
2020-12-09 13:36:41 -0700

    follows tutorial, adds the add/remove page

---
+++ grocery-store/src/main.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+//
+// Vue.config.productionTip = false;
+//
+// // new Vue({
+// //   router,
+// //   render: h => h(App)
+// // }).$mount("#app");
+// //
+
+
+
+
+import mock from './mock-data.js'
+
+let data = {
+  products: mock,
+  cart: []
+}
+
+new Vue({
+  router,
+  data,
+  render: h => h(App)
+}).$mount('#app')
---
+++ grocery-store/src/views/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <div class="wrapper">
+  </div>
+  <h1 v-if="this.$root.$data.cart.length == 0">Cart is empty</h1>
+  <CartList :products="products" />
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue"
+export default {
+  name: 'Home',
+  components: {
+    CartList
+  },
+  computed: {
+  products() {
+    return this.$root.$data.cart;
+
+  }
+  },
+}
+</script>
+
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
